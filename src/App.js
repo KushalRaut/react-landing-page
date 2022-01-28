224751ff220a02f559cd3c7288cf4dd48978de2c
KushalRaut <kushal.074bct025@acem.edu.np>
2022-01-28 22:46:48 +0545

    register page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Home from './pages'
 import Signin from './pages/signin'
+import Signup from './components/Register/Signup'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Switch>
         <Route path="/" component={Home} exact />
         <Route path="/signin" component={Signin} />
+        <Route path='/signup' component={Signup} />
       </Switch>
     </Router>
   )
